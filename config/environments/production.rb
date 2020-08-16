e0241677149de0a4b2955c29288d1cdaa8490f37
Valentine <loyalist.scfa@gmail.com>
2020-08-16 23:51:20 +0500

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { host: 'https://fathomless-beach-05289.herokuapp.com/' }
+  Rails.application.routes.default_url_options = config.action_mailer.default_url_options
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
