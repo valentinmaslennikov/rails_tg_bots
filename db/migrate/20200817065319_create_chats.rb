b304978fdac1f8c504f5a2cc309170f1be369735
Valentine <loyalist.scfa@gmail.com>
2020-08-17 13:01:39 +0500

    .

--- db/migrate/20200817065319_create_chats.rb
+++ db/migrate/20200817065319_create_chats.rb
@@ -1,7 +1,7 @@
 class CreateChats < ActiveRecord::Migration[6.0]
   def change
     create_table :chats do |t|
-      t.integer :system_id
+      t.integer :system_id, :limit => 8
       t.boolean :enabled
 
       t.timestamps
