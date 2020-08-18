1dc6d6885995beea51c367679aed62e185c8c265
Valentine <loyalist.scfa@gmail.com>
2020-08-18 23:10:09 +0500

    .

--- app/controllers/telegram_rust_controller.rb
+++ app/controllers/telegram_rust_controller.rb
@@ -101,7 +101,7 @@ class TelegramRustController < Telegram::Bot::UpdatesController
     end
   end
 
-  def punish(name = nil, term = nil, *)
+  def punish!(name = nil, term = nil, *)
     if name.present? && term.present?
       Prisoner.find_or_create_by!(username: name) do |t|
         t.term = term
