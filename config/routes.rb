e5f591310563b390c7c816abb0234f2135be82c4
Valentine <loyalist.scfa@gmail.com>
2020-08-19 01:34:50 +0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   telegram_webhook TelegramTeaController, :tea_bot
-  telegram_webhook TelegramRustController, :rust
+  telegram_webhook TelegramRustController, :upgrade
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#root'
 
