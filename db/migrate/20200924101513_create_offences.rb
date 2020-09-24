bc7f63aa8e12c0f9261159a76ca17c5649f9d40c
Valentine <loyalist.scfa@gmail.com>
2020-09-24 15:25:52 +0500

    .

--- db/migrate/20200924101513_create_offences.rb
+++ db/migrate/20200924101513_create_offences.rb
@@ -2,7 +2,7 @@ class CreateOffences < ActiveRecord::Migration[6.0]
   def change
     create_table :offences do |t|
       t.text :text
-      t.int :username, limit: 8
+      t.integer :username, limit: 8
 
       t.timestamps
     end
