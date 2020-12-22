a6d12a60ca41f60731a8e74426196e26506502e1
Valentine <loyalist.scfa@gmail.com>
2020-12-22 11:23:16 +0500

    fix

--- app/controllers/telegram_rust_controller.rb
+++ app/controllers/telegram_rust_controller.rb
@@ -40,7 +40,7 @@ class TelegramRustController < Telegram::Bot::UpdatesController
       respond_with :message, text: PT.sample
       return
     end
-    if @user.username!='loyalistscfa' && message['text'] && message['text'].match(/(пид[оа]р|сука|сучара|бля[тд]?ь?|[хx]уй|[хx]у[ёе]|[хx]ул[ие]|пизд|за[её]ба|[её]ба[нт]|хуи[лщ][ае]|хyй)/i)
+    if false#@user.username!='loyalistscfa' && message['text'] && message['text'].match(/(пид[оа]р|сука|сучара|бля[тд]?ь?|[хx]уй|[хx]у[ёе]|[хx]ул[ие]|пизд|за[её]ба|[её]ба[нт]|хуи[лщ][ае]|хyй)/i)
       bot.delete_message(chat_id: chat['id'], message_id: message['message_id'])
       Offence.create!(text: message['text'], username: message['from']['username'])
       respond_with :message, text: 'https://fathomless-beach-05289.herokuapp.com/leha228.jpg'
