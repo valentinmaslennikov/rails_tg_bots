79814133b0482238c4bc950a8d32fdf2f7260ee5
Valentine <loyalist.scfa@gmail.com>
2021-04-06 01:41:23 +0500

    .

--- app/controllers/telegram_hideo_controller.rb
+++ app/controllers/telegram_hideo_controller.rb
@@ -1,10 +1,9 @@
 
 class TelegramHideoController < Telegram::Bot::UpdatesController
-  before_action :init
 
   def twit!
-    twits = @client.user_timeline(117652722, count: 15)
-    l  = -> twit { Tweet.find_by(tweet_id: twit.id).nil? }
+    twits = client.user_timeline(Tweet::HIDEO_TIMELINE_ID, count: 15)
+    l  = -> twit { Tweet.find_by(tweet_id: twit.id).blank? }
     new_twit = -> twit { respond_with :message, text: twit['text']; Tweet.create!(tweet_id: twit['id'])}
     new_twits = twits.select &l
     new_twits.map{|i| new_twit.call(i)}
@@ -13,8 +12,8 @@ class TelegramHideoController < Telegram::Bot::UpdatesController
 
   private
 
-  def init
-    @client = Twitter::REST::Client.new do |config|
+  def client
+    @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = Rails.application.credentials[:twitter][:consumer_key]
       config.consumer_secret     = Rails.application.credentials[:twitter][:consumer_secret]
       config.access_token        = Rails.application.credentials[:twitter][:access_token]
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  HIDEO_TIMELINE_ID = 117652722
 end
