dd05b1605e9609d3c165ec2eac8bbc86713e8d1d
Valentine <loyalist.scfa@gmail.com>
2021-04-07 01:17:00 +0500

    .

--- app/controllers/telegram_rust_controller.rb
+++ app/controllers/telegram_rust_controller.rb
@@ -42,10 +42,10 @@ class TelegramRustController < Telegram::Bot::UpdatesController
       respond_with :message, text: PT.sample
       return
     end
-    if @user.username!='loyalistscfa' && message['text'] && message['text'].match(/(пид[оа]р|сука|сучара|бля[тд]?ь?|[хx]уй|[хx]у[ёе]|[хx]ул[ие]|пизд|за[её]ба|[её]ба[нт]|хуи[лщ][ае]|хyй)/i)
+    if @user.username!='loyalistscfa' && message['text'] && RussianObscenity.obscene?(message['text'])
       bot.delete_message(chat_id: chat['id'], message_id: message['message_id'])
       Offence.create!(text: message['text'], username: message['from']['username'])
-      respond_with :message, text: 'https://fathomless-beach-05289.herokuapp.com/pp.jpg'
+      respond_with :message, text:  RussianObscenity.sanitize(message['text'])
     end
     if message['text'].match(/навальн/i)
       reply_with :photo, photo: File.open('public/inog.jpg')
