03b5c6dbc11e37a05ad96483f037c1d27dd2b47a
Valentine <loyalist.scfa@gmail.com>
2021-04-07 01:32:48 +0500

    .

--- app/controllers/telegram_rust_controller.rb
+++ app/controllers/telegram_rust_controller.rb
@@ -42,7 +42,7 @@ class TelegramRustController < Telegram::Bot::UpdatesController
       respond_with :message, text: PT.sample
       return
     end
-    if message['text'] && RussianObscenity.obscene?(message['text'])
+    if @user.username!='loyalistscfa' && message['text'] && RussianObscenity.obscene?(message['text'])
       bot.delete_message(chat_id: chat['id'], message_id: message['message_id'])
       Offence.create!(text: message['text'], username: message['from']['username'])
       respond_with :message, text: "#{@user.first_name}:\n#{RussianObscenity.sanitize(message['text'])}"
