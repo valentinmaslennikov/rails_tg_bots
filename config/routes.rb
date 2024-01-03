f7bf17ee5641b48ee08c0fd1e4b129939714b562
Valentine <loyalist.scfa@gmail.com>
2024-01-04 02:22:04 +0500

    comment whooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  telegram_webhook TelegramSunshineController, :sunshine
-  telegram_webhook TelegramTeaController, :tea_bot
-  telegram_webhook TelegramRustController, :upgrade
-  telegram_webhook TelegramHideoController, :hideo_bot
-  telegram_webhook KubovichController, :kubovich_bot
+  #telegram_webhook TelegramSunshineController, :sunshine
+  #telegram_webhook TelegramTeaController, :tea_bot
+  #telegram_webhook TelegramRustController, :upgrade
+  #telegram_webhook TelegramHideoController, :hideo_bot
+  #telegram_webhook KubovichController, :kubovich_bot
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#root'
 
